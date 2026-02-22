ea8f0d6bb8829606f72576f3d2bcd66c7353158e
isaacgreenfield <ivtim708@gmail.com>
2026-02-22 17:10:45 +0300

    compiler fix number one

--- BiList.h
+++ BiList.h
@@ -21,19 +21,41 @@ struct sum {
   }
 };
 
-template< class T >
-BiList<T>* add(BiList<T>* prev, BiList<T>* next, const T& d);
-template< class T >
-BiList<T>* insert(BiList<T> *ths, const T &d);
+template<class T>
+BiList<T> * add(BiList<T> *prev, BiList<T> *next, const T &d) {
+  return new BiList<T>(d, next, prev);
+}
 
 template< class T >
-BiList<T>* cut(BiList<T>* ths) noexcept;
-template< class T >
-BiList<T>* erase(BiList<T>* ths) noexcept;
-template< class T >
-BiList<T>* clear(BiList<T>* ths, BiList<T>* end) noexcept;
+BiList<T>* insert(BiList<T>* ths, const T& d) {
+  return ths->next = add(ths, ths->next, d);
+}
 
-template < class T, class F >
-F traverse(F f, BiList<T> ths, BiList<T> end);
+template<class T>
+BiList<T> * cut(BiList<T> *ths) noexcept {
+  ths->next->prev = ths->prev;
+  BiList<T>* tmp = ths->next;
+  delete ths;
+  return tmp;
+}
+template<class T>
+BiList<T> * erase(BiList<T> *ths) noexcept {
+  return ths->next = cut(ths->next);
+}
+template<class T>
+BiList<T> * clear(BiList<T> *ths, BiList<T> * end) noexcept {
+  while (ths != end) {
+    ths = cut(ths);
+  }
+  return ths;
+}
+
+template<class T, class F>
+F traverse(F f, BiList<T>* ths, BiList<T>* end) {
+  for (; ths != end; ths = ths->next) {
+    f(ths->next);
+  }
+  return f;
+}
 
 #endif
--- main.cpp
+++ main.cpp
@@ -1,4 +1,28 @@
+#include<iostream>
+#include "BiList.h"
 
 int main() {
+  int nums[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+  int amn = 10;
+
+  BiList<int>* head = new BiList<int>(nums[0], nullptr, nullptr);
+  BiList<int>* ths = head;
+
+  for (size_t i = 1; i < amn; ++i) {
+    ths = insert(ths, nums[i]);
+    ths = ths->next;
+  }
+  BiList<int>* tail = ths->prev;
+
+  sum s;
+  proizved p;
+  s = traverse(s, head, tail);
+  p = traverse(p, head, tail);
+
+  tail = clear(head, tail);
+  delete head;
+
+  std::cout << s.result << " " << p.result << "\n";
+
   return 0;
 }
