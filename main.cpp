b83942c11b5c4a474988d3c835d87843d578adca
isaacgreenfield <ivtim708@gmail.com>
2026-02-27 18:04:51 +0300

    Changed to apply -> FAKE

--- main.cpp
+++ main.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 #include "BiList.h"
+#define NULL 0
 
 int main() {
   int nums[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
   int amn = 10;
 
-  BiList<int>* head = new BiList<int>{nums[0], nullptr, nullptr};
+  BiList<int>* head = new BiList<int>{NULL, nullptr, nullptr};
   BiList<int>* ths = head;
 
-  for (size_t i = 1; i < amn; ++i) {
+  for (size_t i = 0; i < amn; ++i) {
     ths = insert(ths, nums[i]);
   }
 
@@ -16,8 +17,8 @@ int main() {
   BiList<int>* tmp = nullptr;
   sum s;
   proizved p;
-  s = traverse(s, head, tmp);
-  p = traverse(p, head, tmp);
+  s = traverse(s, head->next, tmp);
+  p = traverse(p, head->next, tmp);
 
   std::cout << s.result << " " << p.result << "\n";
   head = clear(head, tmp);
